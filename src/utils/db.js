4b5e6c8bee2a80c41f049a4b3c6620516f90d835
Tyson Line <123592329+TysonLine@users.noreply.github.com>
2025-01-18 16:44:03 -0500

    MongoDB Atlas Working

---
+++ src/utils/db.js
@@ -0,0 +1,18 @@
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv'; // ES module import for dotenv
+dotenv.config();
+
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri);
+
+async function connectDB() {
+    try {
+        await client.connect();
+        console.log('Connected to MongoDB Atlas!');
+    } catch (err) {
+        console.error('Error connecting to MongoDB Atlas:', err);
+        throw err;
+    }
+}
+
+export default { connectDB, client }; // ES module export
