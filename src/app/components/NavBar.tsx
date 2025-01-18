01ed8d066578365c1a621331a633ee7c34e49a5f
DanThePanMan <danielchenschw@gmail.com>
2025-01-18 12:01:09 -0500

    Create basic nav

---
+++ src/app/components/NavBar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function NavBar() {
+    return (
+        <div className="bg-white border-b-2 h-12 m-0 text-black flex flex-row justify-end items-center p-8">
+            navBar
+        </div>
+    );
+}
