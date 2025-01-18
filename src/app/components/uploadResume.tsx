4714f93285396059f823fb03bb1a16d58cb39b8c
Ansh <anshkakkar05@gmail.com>
2025-01-18 11:17:11 -0500

    Resume Branch

--- src/app/components/uploadResume.tsx
+++ src/app/components/uploadResume.tsx
@@ -1,9 +1,22 @@
+'use client';
 import React from 'react'
+import { useState } from 'react';
 
-const uploadResume = () => {
+const UploadResume = () => {
+  const [file, setFile] = useState<File | null>(null);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files[0]) {
+      setFile(event.target.files[0]);
+    }
+  };
+
   return (
-    <div>uploadResume</div>
-  )
-}
+    <div>
+      <input type="file" accept="application/pdf" onChange={handleFileChange} />
+      {file && <p>Uploaded file: {file.name}</p>}
+    </div>
+  );
+};
 
-export default uploadResume
+export default UploadResume;
