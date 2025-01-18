6f2981395e564f5bf5abf7e2b18af62d471efa35
Ansh <anshkakkar05@gmail.com>
2025-01-18 18:03:26 -0500

    Resume Embeddings work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
 import NavBar from "./components/NavBar";
+import UploadResume from "./components/uploadResume";
 
 export default function Home() {
     return (
@@ -8,6 +9,7 @@ export default function Home() {
             <NavBar />
             <div className="bg-white grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
                 <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start"></main>
+                <UploadResume />
             </div>
         </>
     );
