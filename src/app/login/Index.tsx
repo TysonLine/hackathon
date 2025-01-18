51c0bfe9e7d5b115a60c239f76eac113de358022
DanThePanMan <danielchenschw@gmail.com>
2025-01-18 11:38:51 -0500

    feat: Create login and sign up pages

--- src/app/login/Index.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Index() {
-    return <div className="w-screen h-screen bg-red-400">Index</div>;
-}
---
+++ src/app/newAccount/page.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Card from "../components/card";
+
+//todo: form validation for confirm password, passwords must match
+
+export default function Index() {
+    return (
+        <div className="w-screen h-screen bg-gradient-to-br from-[#FF7621] to-[#FFAF02] flex justify-center items-center">
+            <Card className="flex flex-col justify-center items-center gap-6">
+                <h1 className="text-3xl font-bold">New Account</h1>
+                <p>
+                    Have an account?{" "}
+                    <a className="underline text-secondary" href="/login">
+                        Log in
+                    </a>
+                </p>
+                <input
+                    type="text"
+                    placeholder="Email"
+                    className="input input-bordered w-full max-w-xs"
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    className="input input-bordered w-full max-w-xs"
+                />
+                <input
+                    type="password"
+                    placeholder="Confirm Password"
+                    className="input input-bordered w-full max-w-xs"
+                />
+                <button className="btn bg-primary hover:bg-secondary btn-wide">
+                    Create Account
+                </button>
+            </Card>
+        </div>
+    );
+}
